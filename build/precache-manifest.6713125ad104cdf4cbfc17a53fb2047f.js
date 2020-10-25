a24a2cf61c5d030f64a855edda97f309a6eb1b1b
xiangshanhu <956682073@qq.com>
2020-10-25 14:49:10 +0800

    route
    Signed-off-by: xiangshanhu <956682073@qq.com>

---
+++ build/precache-manifest.6713125ad104cdf4cbfc17a53fb2047f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b530ea61c33936924afa42e81866ad4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f2ee31dbafaf949df94",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "3f2ee31dbafaf949df94",
+    "url": "/static/js/2.233cd5c3.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.233cd5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b344245946025bc42730",
+    "url": "/static/js/main.c93a50ce.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
